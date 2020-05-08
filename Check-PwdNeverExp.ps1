2e19235a271dd4e5f3dc112057225884e412c9f8
Michał <michal.fied@outlook.com>
2020-05-08 13:34:12 +0200

    updated

--- Check-PwdNeverExp.ps1
+++ Check-PwdNeverExp.ps1
@@ -1,4 +1,5 @@
-$input = ".\users1.csv"
+#$input = ".\users1.csv"
+$input = Read-Host "File Path"
 
 Import-Csv -Path $input | ForEach-Object {
 Get-ADUser $_.name -Properties * | Select-Object Name,PasswordNeverExpires,LockedOut,@{n='pwdLastSet';e={[DateTime]::FromFileTime($_.pwdLastSet)}}
