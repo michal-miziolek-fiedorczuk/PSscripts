fe247943f8af6233e779908bb5129f4e5f47c338
Michał <michal.fied@outlook.com>
2020-10-06 10:02:44 +0200

    change how to provide path to file.

--- Add-MultiUsersToGroup.ps1
+++ Add-MultiUsersToGroup.ps1
@@ -1,10 +1,13 @@
-$inputfile =  ".\users.csv"
+$pathToFile = Read-Host "Please provide path to the file."
 
-Import-Csv -Path $inputfile | ForEach-Object { 
+Import-Csv -Path $pathToFile -Delimiter ";" | ForEach-Object {
+$Group = $_.group
+$UserToAdd = $_.username     
 Try {
-    Add-ADGroupMember -Identity GROUPNAME -Members $_.name
-
-} Catch { 
-    Write-Host $_
-}
+        Add-ADGroupMember -Identity $Group -Members $UserToAdd
+        Write-Host -ForegroundColor Green "$UserToAdd has been added to the group $Group"
+    } 
+Catch {
+        Write-Host $_       
+    }
 }
