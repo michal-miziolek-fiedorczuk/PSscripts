f21b9dfd40dac3bf5deeacd3a8ecfe4213bc3180
Michał <michal.fied@outlook.com>
2020-10-15 15:19:25 +0200

    remove file

--- AD-Script-CheckPasswordNeverExpire.ps1
+++
@@ -1,5 +0,0 @@
-$inputFilePath = Read-Host "Type file Path"
-
-Import-Csv -Path $inputFilePath | ForEach-Object {
-Get-ADUser $_.name -Properties * | Select-Object Name,PasswordNeverExpires,LockedOut,@{n='pwdLastSet';e={[DateTime]::FromFileTime($_.pwdLastSet)}}
-}
