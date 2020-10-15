d94be43647a5a5665582598b891cfdbe403bcddc
Michał <michal.fied@outlook.com>
2020-10-15 15:19:12 +0200

    rename file

---
+++ ADScript-CheckPasswordNeverExpire.ps1
@@ -0,0 +1,5 @@
+$inputFilePath = Read-Host "Type file Path"
+
+Import-Csv -Path $inputFilePath | ForEach-Object {
+Get-ADUser $_.name -Properties * | Select-Object Name,PasswordNeverExpires,LockedOut,@{n='pwdLastSet';e={[DateTime]::FromFileTime($_.pwdLastSet)}}
+}
